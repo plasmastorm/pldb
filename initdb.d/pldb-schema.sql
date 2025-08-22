3e7954c6ba9a4fbeab63a321512ad1949e6fab34
Gillian Gunson <ggunson@users.noreply.github.com>
2025-08-21 23:15:47 -0700

    put things in wrong places

--- initdb.d/pldb-schema.sql
+++ initdb.d/pldb-schema.sql
@@ -1,7 +1,8 @@
 CREATE TABLE artists (
     id int NOT NULL AUTO_INCREMENT,
     name varchar(255),
-    PRIMARY KEY (id)
+    PRIMARY KEY (id),
+    UNIQUE KEY (name)
 );
 
 CREATE TABLE tracks (
@@ -9,7 +10,8 @@ CREATE TABLE tracks (
     title varchar(255),
     artist_id int,
     year int,
-    PRIMARY KEY (id)
+    PRIMARY KEY (id),
+    KEY ind_artist (artist_id)
 );
 
 CREATE TABLE shows (
@@ -17,14 +19,18 @@ CREATE TABLE shows (
     theme varchar(255),
     airdate date,
     published boolean,
-    archivelink varchar(255),
-    PRIMARY KEY (id)
+    archivelink varchar(2048),
+    applemusiclink varchar(2048),
+    spotifylink varchar(2048),
+    PRIMARY KEY (id),
+    KEY ind_theme (theme)
 );
 
 CREATE TABLE suggesters (
     id int NOT NULL AUTO_INCREMENT,
     handle varchar(255),
-    PRIMARY KEY (id)
+    PRIMARY KEY (id),
+    UNIQUE KEY ind_handle (handle)
 );
 
 CREATE TABLE plays (
@@ -33,5 +39,8 @@ CREATE TABLE plays (
     track_id int,
     suggester_id int,
     comment varchar(255),
-    PRIMARY KEY (id)
+    PRIMARY KEY (id),
+    UNIQUE KEY ind_show_track (show_id, track_id),
+    KEY ind_track (track_id),
+    KEY ind_suggester (suggester_id)
 );
